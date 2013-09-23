812a22c315768ee07a970dd78d5fc8d27d1f23f4
Jim Pollock <jp@pollockprints.com>
2013-09-23 12:59:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planetesme::Application.config.secret_token = '5158d98fa01553d945e97a689ce014a6f334cefb28a759e31c2217dbe1c458d4a759cfeb050ad2571f8b8de212da4f2ae79e179a2b9ca972a4f1bb3dc443023e'
